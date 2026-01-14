62b12b2e5f4c30a2952dc0a793d9f6fc38b62892
krizsakkornelkaroly <krizsakkornelkaroly@turr.hu>
2026-01-14 08:28:17 +0100

    basic structure 2

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const ejs = require('ejs');
+const path = require('path')
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000 (http://localhost:3000)');
+});
